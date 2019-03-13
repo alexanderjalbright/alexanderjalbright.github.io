1173b80d7874e7edab060e75c2591ccacfa8268c
Alex Albright <alexanderjalbright@gmail.com>
2019-03-13 15:00:17 -0400

    Scroll snapping works and the dropdowns were not lost in the process

--- main.js
+++ main.js
@@ -3,24 +3,6 @@ var logoText = logo.querySelector("h2");
 logo.onmouseover = function(){ logoText.innerHTML = "alexander j albright"};
 logo.onmouseout = function(){ logoText.innerHTML = "alexanderjalbright"};
 
-// var dropdowns = document.querySelectorAll(".dropdown");
-// var choices = [];
-// dropdowns.forEach(function(dropdown){
-//     choices.push(dropdown.querySelectorAll(".choice"));
-//     choices.forEach(function(choice){
-//         dropdown.onmouseover = function(){
-//             choice.forEach(function(item){
-//                 item.style.display = "block";
-//             }); 
-//         }; 
-//         dropdown.onmouseout = function(){
-//             choice.forEach(function(item){
-//                 item.style.display = "none";
-//             });
-//         };      
-//     });
-// });
-
 var dropdowns = document.querySelectorAll(".dropdown");
 dropdowns.forEach(function(dropdown){
     var content = dropdown.querySelector(".content");
