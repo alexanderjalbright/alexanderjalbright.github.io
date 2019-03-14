5f6256259d1db71322ffb5ab473bdb1af51f70d4
Alex Albright <alexanderjalbright@gmail.com>
2019-03-13 20:09:03 -0400

    home section is styled and nav toggle is working

--- main.js
+++ main.js
@@ -1,19 +1,58 @@
-var logo = document.querySelector(".logo");
-var logoText = logo.querySelector("h2");
+const logo = document.querySelector(".logo");
+const logoText = logo.querySelector("h2");
 logo.onmouseover = function(){ logoText.innerHTML = "alexander j albright"};
 logo.onmouseout = function(){ logoText.innerHTML = "alexanderjalbright"};
 
-var dropdowns = document.querySelectorAll(".dropdown");
+const navLock = document.querySelector(".nav-lock");
+const navUnlock = document.querySelector(".nav-unlock");
+const nav = document.querySelector("nav");
+const dropdowns = document.querySelectorAll(".dropdown");
+const sections = document.querySelectorAll("section");
+const main = document.querySelector("main");
+
+navLock.onclick = function(){
+    navLock.style.display = "none";
+    navUnlock.style.display = "block";
+    nav.style.width = "10px";
+    dropdowns.forEach(function(dropdown){
+        dropdown.style.display = "none";
+    });
+    logo.style.display = "none";
+    sections.forEach(function(section){
+        section.style.margin = "0 auto";
+        section.style.height = "100vh";
+    });
+    main.style.height = "100vh";
+    main.style.marginTop = "0";
+}
+
+navUnlock.onclick = function(){
+    navUnlock.style.display = "none";
+    navLock.style.display = "block";
+    nav.style.width = "100vw";
+    dropdowns.forEach(function(item){
+        item.style.display = "block";
+    });
+    logo.style.display = "block";
+
+    sections.forEach(function(section){
+        section.style.margin = "0 auto 18vh auto";
+        section.style.height = "82vh";
+    });
+    main.style.height = "92vh";
+    main.style.marginTop = "var(--nav-height)";
+}
+
+
 dropdowns.forEach(function(dropdown){
-    var content = dropdown.querySelector(".content");
-    
-        dropdown.onmouseover = function(){
-            content.style.display = "block";
-        }; 
-        dropdown.onmouseout = function(){
-            content.style.display = "none";
-        };
+    const content = dropdown.querySelector(".content");
+    dropdown.onmouseover = function(){
+        content.style.display = "block";
+    }; 
+    dropdown.onmouseout = function(){
+        content.style.display = "none";
+    };
 });
 
-
+// document.documentElement.style.setProperty('--nav-height', '4vh');
 
